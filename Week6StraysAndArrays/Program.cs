396e0792598e249ef98eccb9cb2e50ed976d49e2
yavuz06 <yavuzselmkaya@gmail.com>
2023-05-17 15:33:39 +0300

    week6

---
+++ Week6StraysAndArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿string userName = "yavuz";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[4]}");
+
+//int counter = 0;
+
+//for (int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i]== 'a')
+//    {
+//        counter++;
+//    }
+//}
+
+//Console.WriteLine($"Your first name has {counter} a-letters ");
+
+
+string updatedUserName = userName.Replace('a', '2').Replace('H', '1');
+Console.WriteLine(updatedUserName);
